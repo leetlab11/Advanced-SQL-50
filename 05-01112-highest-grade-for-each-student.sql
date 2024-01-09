3da19e1eb693c3b8f31574a1d7cc73c05551378b
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-09 18:15:05 -0500

    Update 05-01112-highest-grade-for-each-student.sql

--- 05-01112-highest-grade-for-each-student.sql
+++ 05-01112-highest-grade-for-each-student.sql
@@ -7,8 +7,18 @@ from
 where rnk = 1
 order by 1
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- nested 
+-- first get id and highest grade, then get min course_id
+
+select student_id, min(course_id) as course_id, grade
+from Enrollments
+where (student_id, grade) in
+    (select student_id, max(grade) as grade
+    from Enrollments
+    group by student_id)
+group by student_id
+order by student_id
 
 -- amazon- 2
 -- coursera- 1
-
---------------------------------------------------------------------------------------------------------------------------------------------------------------
