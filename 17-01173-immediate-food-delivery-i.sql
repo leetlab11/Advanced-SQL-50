5dbfebdbf34096bb701ce9c3705f60d1e092acd7
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 18:08:26 -0500

    Update 17-01173-immediate-food-delivery-i.sql

--- 17-01173-immediate-food-delivery-i.sql
+++ 17-01173-immediate-food-delivery-i.sql
@@ -1,1 +1,12 @@
+-- simple condition in aggregate function- count immediate, divide by total rows in the table
 
+select round(sum(order_date = customer_pref_delivery_date) / count(*) * 100, 2) as immediate_percentage
+from Delivery
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- same as above but using case
+
+select round(sum( case when order_date = customer_pref_delivery_date then 1 else 0 end) / count(*) * 100, 2) as immediate_percentage
+from Delivery
+
+
+-- doordash- 2
