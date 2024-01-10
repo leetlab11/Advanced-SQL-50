3fde977346bfc650ebec706a10111deb03fd7e9f
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 18:12:50 -0500

    Update 16-01741-find-total-time-spent-by-each-employee.sql

--- 16-01741-find-total-time-spent-by-each-employee.sql
+++ 16-01741-find-total-time-spent-by-each-employee.sql
@@ -1,1 +1,9 @@
+-- simple aggregation- we need total sum of the diff between out and in time
 
+select event_day as day, emp_id, sum(out_time-in_time) as total_time
+from Employees
+group by 1, 2
+
+
+-- adobe- 2
+-- amazon- 1
