cff8c04f33096fbf3307e0d5c422c13e2d0617ff
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 15:04:14 -0500

    Update 08-01407-top-travellers.sql

--- 08-01407-top-travellers.sql
+++ 08-01407-top-travellers.sql
@@ -1,1 +1,11 @@
+-- using ifnull around sum()- can also use coalesce
 
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance
+from Users u
+left join Rides r
+on u.id = r.user_id
+group by u.id
+order by 2 desc, 1 asc
+
+
+-- point72- 1
