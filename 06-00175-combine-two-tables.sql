01e396ec0c0263e7a95443a082f60d64e4634b3f
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 14:34:21 -0500

    Update 06-00175-combine-two-tables.sql

--- 06-00175-combine-two-tables.sql
+++ 06-00175-combine-two-tables.sql
@@ -1,1 +1,14 @@
+-- simple LEFT JOIN
 
+select p.firstName, p.lastName, a.city, a.state
+from Person p
+left join Address a
+using(personId)
+
+
+-- apple- 4
+-- bloomberg- 2
+-- amazon- 2
+-- microsoft- 2
+-- adobe- 3
+-- google- 3
