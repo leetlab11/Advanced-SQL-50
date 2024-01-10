acd4ddb036eba0de3e0d66a6ff66c8c6b76a5f78
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 18:17:14 -0500

    Update 15-00586-customer-placing-the-largest-number-of-orders.sql

--- 15-00586-customer-placing-the-largest-number-of-orders.sql
+++ 15-00586-customer-placing-the-largest-number-of-orders.sql
@@ -1,1 +1,14 @@
+-- the the cusotmer with maximum order count, order by, limit
 
+select customer_number
+from Orders
+group by 1
+order by count(order_number) desc
+limit 1
+
+
+-- adobe- 2
+-- google- 3
+-- apple- 2
+-- uber- 2
+-- twitter- 1
