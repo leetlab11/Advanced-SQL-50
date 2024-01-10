fa1bdf5ae01d4845bf43e494e5c4d7ff64f3c632
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 17:34:14 -0500

    Update 14-01571-warehouse-manager.sql

--- 14-01571-warehouse-manager.sql
+++ 14-01571-warehouse-manager.sql
@@ -1,1 +1,23 @@
+-- multiple units, width, length, height, then calculate sum() of those
 
+select w.name as warehouse_name, 
+sum(units * Width * Length * Height) as volume
+from Warehouse w
+left join Products p
+on w.product_id = p.product_id
+group by 1
+
+-------------------------------------------------------------------------------------------------------------
+-- breaking down the above one
+  
+with CTE as (
+    select product_id, (Width * Length * Height) as size
+    from Products)
+
+select name as warehouse_name, sum(units * size) as volume
+from Warehouse w
+left join CTE c
+on c.product_id = w.product_id
+group by name
+
+-- amazon- 1
