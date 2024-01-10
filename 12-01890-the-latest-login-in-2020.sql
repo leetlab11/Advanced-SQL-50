b89889810dd1d55f8cc4d85c6d665d5eac294de3
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-10 17:11:40 -0500

    Update 12-01890-the-latest-login-in-2020.sql

--- 12-01890-the-latest-login-in-2020.sql
+++ 12-01890-the-latest-login-in-2020.sql
@@ -1,1 +1,21 @@
+select user_id, max(time_stamp) as last_stamp
+from Logins
+where time_stamp like '2020%'
+group by 1
 
+----------------------------------------------------------------------------------------------------------------
+-- using year() for getting 2020 instead of like
+
+select user_id, max(time_stamp) as last_stamp
+from Logins
+where year(time_stamp) = '2020'
+group by 1
+
+----------------------------------------------------------------------------------------------------------------
+-- using first_value()
+
+select distinct user_id, first_value(time_stamp) over(partition by user_id order by time_stamp desc) as last_stamp
+from Logins
+where year(time_stamp) = '2020'
+
+-- no companies listed
