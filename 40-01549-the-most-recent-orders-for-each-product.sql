61fc6fb36bfaf07362ed4f5074bc59be3d9ad3ac
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-01-24 09:00:35 -0500

    Update 40-01549-the-most-recent-orders-for-each-product.sql

--- 40-01549-the-most-recent-orders-for-each-product.sql
+++ 40-01549-the-most-recent-orders-for-each-product.sql
@@ -1,4 +1,4 @@
--- pending explanation and subquery approach01549-
+-- using window function- dense_rank() to get most recent orders by date, partition by product_id
 
 with CTE as
     (select p.product_name, o.product_id, o.order_id, o.order_date,
@@ -10,4 +10,16 @@ with CTE as
 select product_name, product_id, order_id, order_date
 from CTE
 where rnk = 1
+order by 1, 2, 3
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- using subquery
+    
+select p.product_name, o.product_id, o.order_id, o.order_date
+from Products p 
+join Orders o
+on p.product_id = o.product_id
+where (o.product_id, o.order_date) in (select product_id, max(order_date)
+                                        from Orders
+                                        group by product_id)
 order by 1, 2, 3
